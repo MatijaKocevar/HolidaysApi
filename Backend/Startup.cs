8a4fe603d9067674225cc64b5f8a62ed84c95f25
MatijaKocevar <matija.kocev@gmail.com>
2021-05-14 11:43:46 +0200

    Connected backend to frontend & created table

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -29,6 +29,8 @@ namespace Holidays
 
             //dependency injection - New object per client request
             services.AddScoped<IHolidayRepo, SqlHolidayRepo>();
+
+            services.AddCors(options => options.AddDefaultPolicy(builder => builder.AllowAnyOrigin()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +44,8 @@ namespace Holidays
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="text-center">
+      <!-- simple form to input year -->
+      <form method="get">
+        <h1>Enter year to get holidays</h1>
+        <input type="text" /><br />
+        <input type="submit" />
+      </form>
+
+      <!-- table for displaying data -->
+      <table class="holidaysTable">
+        <tr>
+          <th>Holiday</th>
+          <th>Date</th>
+          <th>Weekday</th>
+        </tr>
+        <tr v-for="item of holidays" :key="item.id">
+          <td>{{ item.name }}</td>
+          <td>{{ item.day + "." + item.month + "." + item.year }}</td>
+          <td>{{ item.weekday }}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const URL = "https://localhost:5001/api/holidays";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      holidays: [],
+    };
+  },
+  async mounted() {
+    const res = await fetch(URL).then((response) => response.json());
+    console.log(res);
+    return (this.holidays = res);
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  background-color: #303030;
+  color: #fff;
+}
+
+.holidaysTable tr td {
+  border: 1px solid #fff;
+}
+
+.holidaysTable {
+  margin-top: 100px;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
